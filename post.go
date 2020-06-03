f2cf9bbc1a9024277b0ad7e55a3561acc1f1bd9f
Scott Ware <scottdware@gmail.com>
2020-06-03 10:23:38 -0400

    Added POST function

---
+++ post.go
@@ -0,0 +1,97 @@
+package bluecat
+
+import (
+	"fmt"
+
+	"gopkg.in/resty.v1"
+)
+
+// addACL
+
+// addAccessRight
+
+// addAdditionalIPAddresses
+
+// addAliasRecord
+
+// addBulkHostRecord
+
+// addCustomOptionDefinition
+
+// addDHCP4RangeBySize
+
+// addDHCP4Range
+
+// addDHCP6ClientDeploymentOption
+
+// addDHCP6RangeBySize
+
+// addDHCP6Range
+
+// addDHCP6ServiceDeploymentOption
+
+// addDHCPClientDeploymentOption
+
+// addDHCPDeploymentRole
+
+// addDHCPServiceDeploymentOption
+
+// addDHCPSubClass
+
+// addDHCPVendorDeploymentOption
+
+// addDNSDeploymentOption
+
+// addDNSDeploymentRole
+
+// addDeviceInstance
+
+// addDeviceSubtype
+
+// addDeviceType
+
+// addDevice
+
+// addEntity
+
+// addEnumNumber
+
+// addEnumZone
+
+// addExternalHostRecord
+
+// addGenericRecord Adds generic records.
+//
+// Parameter `absolutename` is the FQDN of the record. If you are adding a record in a zone that is linked to a incremental
+// naming policy, you must add a single hash sign (#) at the appropriate location in the FQDN. Depending on the policy order value,
+// the location of the single hash sign varies.
+//
+// Parameter `properties` adds object properties, including comments and user-defined fields. Parameter `rdata` is the data for the
+// resource record, in BIND format. For example, A records follow the format 10.0.0.4. Parameter `ttl` is the time-to-live (TTL) value
+// for the record. To ignore the TTL, set this value to -1.
+//
+// Parameter `objecttype` is the type of record. Valid settings for this parameter are the generic resource record types supported in
+// Address Manager: A6, AAAA, AFSDB, APL, CAA, CERT, DHCID, DNAME, DNSKEY, DS, ISDN, KEY, KX, LOC, MB, MG, MINFO, MR, NS, NSAP, PX, RP,
+// RT, SINK, SSHFP, TLSA, WKS, and X25.
+//
+// Parameter `viewid` is the object ID for the parent view to which you are adding the record.
+//
+// Returns the object ID for the new generic resource record.
+func (b *Bluecat) addGenericRecord(absolutename, properties, rdata string, ttl int64, objecttype string, viewid int64) (string, error) {
+	req := fmt.Sprintf("https://%s%s/addGenericRecord?absoluteName=%s&rdata=%s&ttl=%d&type=%s&viewId=%d&properties=%s",
+		b.Server, b.URI, absolutename, rdata, ttl, objecttype, viewid, properties)
+	resp, err := resty.R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Authorization", fmt.Sprintf("%s", b.AuthToken)).
+		Post(req)
+
+	if err != nil {
+		return "", fmt.Errorf("%s - addGenericRecord request", err)
+	}
+
+	// if err := json.Unmarshal([]byte(resp.String()), &results); err != nil {
+	// 	return nil, fmt.Errorf("%s - GetEntitiesByName JSON parse", err)
+	// }
+
+	return resp.String(), nil
+}
